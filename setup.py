602bd8215a982cbf1cef8553232f28e167367be9
Keshav Singh Rathore <keshav111996@gmail.com>
2026-02-04 19:45:22 +0530

    requirements and installation part done

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+REPO_NAME = "Nanomaterials-Journal-Summariser"
+AUTHOR_USER_NAME = "Data-ragpicker"
+SRC_REPO = "JournalSummariser"
+AUTHOR_EMAIL = "keshav111996@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A project to summarise nanomaterials research articles using NLP techniques.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+
+    packages=setuptools.find_packages(where="src")
+)
